236db1616dd84744de9769243e887b932f62d68f
abdkhaleel <abdkhaleel16@gmail.com>
2026-04-22 12:18:17 +0530

    navbar layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           color: 'var(--charcoal, #2D2D2D)',
         }}
       >
-        <Navbar />
+        {/* <Navbar /> */}
         
         <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {children}
